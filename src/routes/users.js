9079d2da9532c3906a26d33cee6164104bb83ce4
mokuzaru <kenny.otoya@pucp.pe>
2019-07-17 08:42:23 -0500

    signup

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,12 +1,43 @@
 const express = require('express');
 const router = express.Router();
 
+const User = require('../models/User');
+
 router.get('/users/signin', (req, res) =>{
   res.render('users/signin');
 });
 
 router.get('/users/signup', (req, res) =>{
   res.render('users/signup');
+});
+
+router.post('/users/signup',async (req, res) => {
+  const {name, email, password, confirm_password} = req.body;
+  const errors = [];
+  if(name.length <= 0){
+    errors.push({text:'Please inser your Name'});
+  }
+  if(password != confirm_password){
+    errors.push({text: 'Password do not match'});
+  }
+  if(password.length < 4) {
+    errors.push({text: 'Password must be at least 4 characters'});
+  }
+  if(errors.length > 0){
+    res.render('users/signup', {errors, name, email, password, confirm_password});
+  }else{
+    const emailUser = await User.findOne({email: email});
+    if(emailUser){
+      req.flash('error_msg', ' The email has been taking');
+      res.redirect('/users/signup');
+    } else {
+      const newUser = new User({name, email, password});
+      newUser.password = await newUser.encryptPassword(password)
+      await newUser.save();
+      req.flash('success_msg', 'You are register');
+      res.redirect('/users/signin');
+    }
+  }
 });
 
 module.exports = router;
