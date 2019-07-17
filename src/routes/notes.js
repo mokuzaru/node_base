3f1236d5826ec0ccd90e753fc997851c943f7685
mokuzaru <kenny.otoya@pucp.pe>
2019-07-17 03:27:07 -0500

    add connection to db and form new notes

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -1,8 +1,31 @@
 const express = require('express');
 const router = express.Router();
 
+const Note = require('../models/Note');
 router.get('/notes/add', (req,res) =>{
-  res.render('notes/new-notes.hbs')
+  res.render('notes/new-notes')
+});
+
+router.post('/notes/new-notes', async(req,res) =>{
+  const {title, description} = req.body;
+  const errors = [];
+  if(!title){
+    errors.push({text: 'Please Write a Title'});
+  }
+  if(!description){
+    errors.push({text: 'Please Write a Title'});
+  }
+  if(errors.length > 0){
+    res.render('notes/new-notes',{
+      errors,
+      title,
+      description
+    });
+  }else{
+    const newNote = new Note({title, description});
+    await newNote.save();
+    res.redirect('/notes');
+  }
 });
 router.get('/notes', (req, res) =>{
   res.send('notas');
