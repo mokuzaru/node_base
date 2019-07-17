e6a46c8e28446153c3f861c758b58443b3b1d424
mokuzaru <kenny.otoya@pucp.pe>
2019-07-17 04:58:44 -0500

    update files

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -32,4 +32,15 @@ router.get('/notes', async(req, res) =>{
   res.render('notes/allnotes', { notes });
 });
 
+router.get('/notes/edit/:id', async(req, res) =>{
+  const note = await Note.findById(req.params.id);
+  res.render('notes/edit-note', {note});
+});
+
+router.put('/notes/edit-note/:id', async(req, res) => {
+  const {title, description} = req.body;
+  await Note.findByIdAndUpdate(req.params.id, {title, description});
+  res.redirect('/notes');
+})
+
 module.exports = router;
