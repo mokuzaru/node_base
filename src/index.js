fdb424a2a7f2bf28829fbcc32fdea88354b5274e
mokuzaru <kenny.otoya@pucp.pe>
2019-07-18 02:46:18 -0500

    log in done and navigation fils created

--- src/index.js
+++ src/index.js
@@ -4,9 +4,12 @@ const exphbs = require('express-handlebars');
 const methodOverride = require('method-override');
 const session = require('express-session');
 const flash = require('connect-flash');
+const passport = require('passport');
+
 //Initializations
 const app = express();
 require('./database');
+require('./config/passport')
 //Settings
 app.set('port',process.env.PORT || 4000);
 app.set('views', path.join(__dirname, 'views'));
@@ -26,11 +29,15 @@ app.use(session({
   resave: true,
   saveUninitialized: true
 }));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(flash());
+
 //Global Variables
 app.use((req, res, next) => {
   res.locals.success_msg= req.flash('success_msg');
   res.locals.errors_msg = req.flash('errors_msg');
+  res.locals.error = req.flash('error');
   next();
 });
 //Routes
